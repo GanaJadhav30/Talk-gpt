c542412a0cd272cadacd5fe9d6cc02ccad862513
GanaJadhav30 <jadhavganeshatclg@gmail.com>
2025-07-28 23:43:50 +0530

    add basic css

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,19 @@ function App() {
 
   return (
     <>
-      <div className='bg-zinc-700 w-full min-h-screen'>
+      <div className='bg-zinc-700 w-full flex flex-col  h-screen'>
         <div >
-          <h1>Talk-gpt</h1>
+          <h1 className='font-bold text-2xl text-white p-4'>Talk-gpt</h1>
+        </div>
+        <div className='m-3  flex-1 relative w-full h-screen overflow-hidden'>
+
+          <div className='absolute bottom-0 inset-x-0'>
+          <form>
+            <input className='bg-white rounded p-2 m-2 w-1/2' type="text" />
+            <button className='bg-blue-600 rounded-lg p-2'>Submit</button>
+          </form>
+          </div>
+         
         </div>
       </div>
     </>
